623eb8642915596face02dc398a958259cd5759a
alexanderdombroski <dom21013@byui.edu>
2024-06-22 21:49:43 -0600

    added logos and icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './styles/App.css';
 import Footer from './footer';
+import Header from "./header";
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <header className="App-header">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
--- src/header.tsx
+++ src/header.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 
-export function header() {
-    
+function Header() {
+    return (
+        <header>
+            <Logo/>
+            <Nav/>
+        </header>
+    );
 }
 
-export function nav() {
-
+function Nav() {
+    return (
+        <nav>
+            <ol>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ol>
+        </nav>
+    );
 }
 
+function Logo() {
+    return (
+        <a href="#">
+            <img src="images/portfolio-logo.png" alt="portfolio logo" />
+        </a>
+    );
+}
+
+export default Header;
